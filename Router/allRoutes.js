20aa7cb774cbba04e526b5b0d152092fe5552782
Amel252 <amelghanem25@gmail.com>
2025-03-08 00:43:10 +0100

    Ajout systéme routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,7 @@
+import Route from "./Route.js";
+
+//définir ici vos routes
+export const allRoutes = [new Route("/", "Acceuil", "/pages/home.html")];
+
+//le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
